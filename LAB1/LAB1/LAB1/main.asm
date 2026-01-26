3ed6a4092074bf7b39bbfc47204d38fecbecc6c1
Alexander Thomsen <thomsen81@gmail.com>
2026-01-26 21:34:06 +0100

    more comments

--- LAB1/LAB1/LAB1/main.asm
+++ LAB1/LAB1/LAB1/main.asm
@@ -45,23 +45,25 @@ Data address space:
 LOOP:
    LDI  R17,1           ;R17 = 1 
    ADD  R16,R17         ;R16 = R16 + R17
-   CALL DISP_AND_DELAY  ;Display R16
-   JMP  LOOP            ;Jump to "LOOP"
+   CALL DISP_AND_DELAY  ;Display R16       pushes return address, the addres of the next instruction after call onto the stack and sets PC = address of DISP_AND_DELAY
+   JMP  LOOP            ;Jump to "LOOP"  unconditional JUMP back
 
 ;********** DISPLAY R16 *********
 ;********** AND DELAY ***********
 DISP_AND_DELAY:
-   MOV  R17,R16
-   OUT  PORTB,R17
-   CLR  R17
-   CLR  R18
-   LDI  R19,100
+   MOV  R17,R16		; R17->R16
+   OUT  PORTB,R17	; R17->PORTB (LEDS)
+   CLR  R17			; R17 = 0
+   CLR  R18			; R18 = 0
+   LDI  R19,100		; R19 = 100
 AGAIN:
-   DEC  R17
-   BRNE AGAIN
-   DEC  R18
-   BRNE AGAIN
-   DEC  R19
-   BRNE AGAIN
-   RET	
+   DEC  R17			; R17 = R17 - 1		(0 - 1 -> 255)  Z = 0      
+   BRNE AGAIN		; IF Z != 0 -> AGAIN, else if Z = 0 -> continue   (inner looper)
+   DEC  R18			; R18 = R18 - 1		(0 - 1 -> 255)	Z = 0
+   BRNE AGAIN		; IF Z != 0 -> AGAIN, else if Z = 0 -> continue (loop R17 again)  (mid loop)
+   DEC  R19			; as above 
+   BRNE AGAIN		; as above (outer loop)
+   RET				; pops the return address from the stack back into the PC, so execution resumes from the instruction after the CALL (JMP inst)
 ;********************************
+
+
