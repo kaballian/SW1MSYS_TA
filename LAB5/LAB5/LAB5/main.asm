ec03fc39091e7ebd770cb373d9df6d301a3924e4
Alexander Thomsen <thomsen81@gmail.com>
2026-02-23 20:44:05 +0100

    ex2 complete, except case 2

--- LAB5/LAB5/LAB5/main.asm
+++ LAB5/LAB5/LAB5/main.asm
@@ -72,5 +72,214 @@ R19 = R19 + R23 + C
     ADC R17, R21
     ADC R18, R22
     ADC R19, R23
-//UNSIGNED BTW!
+//UNSIGNED BTW! - only works as unsigned, due to the fact signed is 2.147.483.647 as max value
     
+//Ex 2
+
+//Port A is inputs
+
+    CLR R16
+    OUT DDRA, R16
+   
+//Port B is outputs
+    SER R16
+    OUT DDRB, R16
+//turn off all LEDs
+    CLR R16
+    OUT PORTB, R16
+
+//pool all the PINS
+HERE:
+    //sample inputs ONCE
+
+    IN R20, PINA
+    COM R20
+
+
+    SBRS R20, 7
+    RJMP CASE_6:
+    IN R16, PORTB
+    INC R16
+    OUT PORTB, R16
+    
+CASE_6:
+    SBRS R20, 6
+    RJMP CASE_5
+    IN R16, PORTB
+    DEC R16
+    OUT PORTB, R16
+
+CASE_5:
+    SBRS R20, 5
+    RJMP CASE_4
+    IN R16, PORTB
+    SWAP R16
+    OUT PORTB, R16
+
+
+CASE_4:
+
+    SBRS R20, 4
+    RJMP CASE_3
+    IN R16, PORTB
+    COM R16,
+    OUT PORTB, R16
+
+CASE_3:
+    SBRS R20, 3
+    RJMP CASE_2
+    IN R16, PORTB
+    LDI R17, 4
+SHIFT_LOOP:
+    LSR R16
+    DEC R17
+    BRNE SHIFT_LOOP
+    OUT PORTB, R16
+
+
+CASE_2:
+    SBRS PINA, 2
+    //TODO - ignore for now
+
+CASE_1:
+    SBRS R20, 1
+    RJMP CASE_0
+    IN R16, PORTB
+    ANDI R16, 0b01111110
+    OUT PORTB, R16
+
+CASE_0:
+    SBRS R20, 0
+    JMP MAIN
+    IN R16, PORTB,
+    ORI R16, 0b11111111
+    OUT PORTB, R16
+    
+//SW7 - inc PORTB
+//SW6 - dec PORTB
+//SW5 - value of LED7-4 switches with LED3-0 (use a SWAPF or something)
+//SW4 - complement all LED's
+//SW3 - value of PORTB is divided by 8 (shift down)
+//SW2 - value of PORTB is divided by 7 (actual divison)
+//SW1 - LED7 & 0 are turned off, while others are unaffected
+//SW0 - LED7 & 0 are turned on, while others are unaffected
+
+    //READ PIN A
+
+ 
+    CALL DELAY_1S
+    
+    RJMP HERE
+
+DELAY_1S:
+    LDI R18, 100 ; outer loop
+OUTER:
+    LDI R19, 200 ; middle
+MIDDLE:
+    LDI R20, 200 ; inner
+INNER:
+    DEC R20
+    BRNE INNER
+    DEC R19
+    BRNE MIDDLE
+    DEC R18
+    BRNE OUTER
+    RET
+
+
+
+; // OFFICIAL LØSNING
+
+
+;  HERE:
+;    LDI  R20,2             ;Tænd LED 2
+;    CALL LED_ON
+;    CALL DELAY
+
+;    LDI  R20,7             ;Tænd LED 7
+;    CALL LED_ON
+;    CALL DELAY
+
+;    SER  R16               ;Tænd alle LEDs
+;    OUT  PORTB,R16
+;    CALL DELAY
+
+;    LDI  R20,5             ;Sluk LED 5
+;    CALL LED_OFF
+;    CALL DELAY
+
+;    LDI  R20,0             ;Sluk LED 0
+;    CALL LED_OFF
+;    CALL DELAY
+
+;    LDI  R20,0             ;Toggle LED 0
+;    CALL LED_TOGGLE
+;    CALL DELAY
+
+;    LDI  R20,7             ;Toggle LED 7
+;    CALL LED_TOGGLE
+;    CALL DELAY
+
+;    CLR  R16               ;Sluk alle LEDs
+;    OUT  PORTB,R16
+;    CALL DELAY
+
+;    JMP HERE               ;Gentag loop
+
+; ;************ LED_OFF **********
+; ;***** Slukker en LED på PB ****
+; ;***** Bit nr.(0-7) i R20   ****
+; ;*******************************
+; LED_OFF:
+;    LDI  R21,1             ;R21 = 0b00000001
+;    CPI  R20,0
+;    BREQ KLAR1             ;Hop, hvis LED nr. = 0
+; IGEN1:
+;    LSL  R21               ;Venstre-skift R21
+;    DEC  R20               ;ialt "LED nr." pladser
+;    BRNE IGEN1
+; KLAR1:
+;    COM  R21               ;Inverter "masken" 
+;    IN   R20,PINB          ;Aflæs alle LEDs
+;    AND  R20,R21           ;- lav bitvis AND   
+;    OUT  PORTB,R20         ;- og skriv ud til LEDs igen
+;    RET	
+; ;*******************************
+
+; ;************ LED_ON ***********
+; ;***** Taender en LED på PB ****
+; ;***** Bit nr.(0-7) i R20   ****
+; ;*******************************
+; LED_ON:
+
+;    ;<-------- Skriv den manglende kode her
+
+
+;    RET	
+; ;*******************************
+
+; ;********** LED_TOGGLE *********
+; ;***** Toggler en LED på PB ****
+; ;***** Bit nr.(0-7) i R20   ****
+; ;*******************************
+; LED_TOGGLE:
+
+;    ;<-------- Skriv den manglende kode her
+
+;    RET	
+; ;*******************************
+
+; ;*******************************
+; DELAY:
+;    LDI  R31,130
+;    CLR  R30
+;    CLR  R29
+; LOOP:
+;    DEC  R29
+;    BRNE LOOP
+;    DEC  R30
+;    BRNE LOOP
+;    DEC  R31
+;    BRNE LOOP
+;    RET
+; ;*******************************
