6e0dcec03cf5e06a57b771f52bd248e6787913d1
Alexander Thomsen <thomsen81@gmail.com>
2026-04-13 22:52:40 +0200

    lab11

---
+++ LAB11/LAB11.c
@@ -0,0 +1,71 @@
+/********************************************
+* MSYS, LAB 11, Part 2                      *
+* Test program for Mega2560 UART driver.    *
+* Using UART 0.                             *
+* The LEDs will indicate test progress      *
+*********************************************/
+#include <avr/io.h>
+#include "uart.h"
+#include "led.h"
+
+int main()
+{
+	char tegn;
+	char TestStreng[] = "This is string number 1\r\n";
+	int8_t i;
+
+	// Initialize LED port
+	initLEDport();
+	// Initialize UART: Baud = 9600, 8 data bits, No Parity
+	InitUART();
+
+	while (1)
+	{
+		// Testing SendChar
+		writeAllLEDs(1);
+		SendChar('A');
+		SendChar('B');
+		SendChar('C');
+		SendChar('\r');
+		SendChar('\n');
+
+		// Testing ReadChar() : Read and echo
+		writeAllLEDs(2);
+		tegn = ReadChar();
+		SendChar(tegn);
+		SendChar('\r');
+		SendChar('\n');
+
+		// Testing CharReady() : Wait, read and echo
+		writeAllLEDs(3);
+		while ( !CharReady() )
+		{}
+		SendChar( ReadChar() );
+		SendChar('\r');
+		SendChar('\n');
+
+		// Testing SendString(): Sending string number 1 (SRAM memory)
+		writeAllLEDs(4);
+		SendString(TestStreng);
+
+		// Testing SendString(): Sending string number 2 (Flash memory)
+		writeAllLEDs(5);
+		SendString("This is string number 2\r\n");
+		
+		// Testing SendInteger()
+		writeAllLEDs(6);
+		SendInteger(12345); //Positive number
+		SendChar('\r');
+		SendChar('\n');
+		SendInteger(-1000); //negative number
+		SendChar('\r');
+		SendChar('\n');
+		
+		for (i=-5; i<5; i++)
+		{
+			SendInteger(i);	SendChar(' ');
+		}
+		SendChar('\r');
+		SendChar('\n');
+	}
+}
---
+++ LAB11/LAB11/LAB11/Debug/main.d
@@ -0,0 +1,39 @@
+main.d main.o: .././main.c \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\ATMEL_STUDIO\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom2560.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ .././uart3.h .././LED.h
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\ATMEL_STUDIO\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom2560.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\atmel_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+.././uart3.h:
+
+.././LED.h:
